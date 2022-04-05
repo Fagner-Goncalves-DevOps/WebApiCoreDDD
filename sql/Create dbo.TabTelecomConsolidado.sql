8b1ce2f8eed64d196e3dbc52e9172b50fb87b342
fagner gonçalves <fagnergoncalves15@gmail.com>
2022-04-05 03:57:55 -0300

    Conceitos Basicos DDD - Migrations|ContextDB|Database Sql

---
+++ sql/Create dbo.TabTelecomConsolidado.sql
@@ -0,0 +1,23 @@
+﻿USE [ModeloTelecom]
+GO
+
+/****** Object: Table [dbo].[TabTelecomConsolidado] Script Date: 05/04/2022 03:52:44 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TabTelecomConsolidado] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [Dia]           DATETIME        NOT NULL,
+    [Fila]          INT             NULL,
+    [Terminator]    VARCHAR (100)   NULL,
+    [StatusInicial] VARCHAR (255)   NULL,
+    [StatusFinal]   VARCHAR (100)   NOT NULL,
+    [Disparos]      INT             NOT NULL,
+    [Custo]         DECIMAL (18, 2) NOT NULL,
+    [Servidor]      VARCHAR (100)   NULL
+);
+
+select * from ModeloTelecom.dbo.TabTelecomConsolidado
---
+++ src/Infra.Data/Migrations/20220405064324_inicialDbTelecom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Data.Migrations
+{
+    public partial class inicialDbTelecom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TabTelecomConsolidado",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dia = table.Column<DateTime>(type: "DateTime", nullable: false),
+                    Fila = table.Column<int>(type: "int", nullable: true),
+                    Terminator = table.Column<string>(type: "varchar(100)", nullable: true),
+                    StatusInicial = table.Column<string>(type: "varchar(255)", nullable: true),
+                    StatusFinal = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Disparos = table.Column<int>(type: "int", nullable: false),
+                    Custo = table.Column<decimal>(type: "decimal (18,2)", nullable: false),
+                    Servidor = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabTelecomConsolidado", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TabTelecomConsolidado");
+        }
+    }
+}
